8ee7decc53f3f343d26dda9e6057bfead23b470d
BWGeGe <brz210319@spengergasse.at>
2024-04-09 10:46:42 +0200

    seed.js datensatznamen geändert

--- 2024-04-02__PLF4_LSG/seed.js
+++ 2024-04-02__PLF4_LSG/seed.js
@@ -10,8 +10,8 @@ async function seed() {
     const userCountActual = await prisma.benutzer.count();
     for (let i = 1; i <= userCountTarget - userCountActual; i++) {
         const user = {
-            Fullname: fakerDE.person.fullName(),
-            Email: fakerDE.internet.email(),
+            fullname: fakerDE.person.fullName(),
+            email: fakerDE.internet.email(),
         };
         console.log('seeding');
         const createBenutzer = await prisma.benutzer.create({
@@ -44,7 +44,7 @@ async function seed() {
             data: {
                 name: fakerDE.word.noun(),
                 createdAt: fakerDE.date.recent(),
-                User: {
+                benutzer: {
                     connect: {
                         id: userIds[Math.floor(Math.random() * userIds.length)],
                     },
@@ -70,7 +70,7 @@ async function seed() {
                 ],
             },
             data: {
-                Track: { connect: rndTracklist },
+                tracks: { connect: rndTracklist },
             },
         });
     }
